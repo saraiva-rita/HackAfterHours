375f8fdd087dd5b235283b8d292bbf9642271a30
Rita Saraiva <rita_saraiva@hotmail.com>
2023-08-16 16:19:19 +0100

    landing page

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -7,6 +7,10 @@ const User = require('../models/User.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
+  res.render('landingpage');
+});
+/* GET home page */
+router.get('/index', (req, res, next) => {
   res.render('index');
 });
 
